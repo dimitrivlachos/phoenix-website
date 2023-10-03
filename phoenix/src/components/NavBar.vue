acda2fab11b82c77878709eecc96625e9d367bf2
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-10-03 20:48:07 +0100

    Blank Vue Project

---
+++ phoenix/src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="nav">
+        <h1>Phoenix</h1>
+        <ul>
+        <li>
+            <router-link to="/">Home</router-link>
+        </li>
+        <li>
+            <router-link to="/about">About</router-link>
+        </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
+
+<style scoped>
+.nav {
+    background-color: #1A4589;
+    border-bottom: 1px solid #eee;
+    height: 40px;
+    line-height: 40px;
+    padding: 0 20px;
+}
+</style>
---
+++ phoenix/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/Phoenix_logo.jpg" />
+    <HelloWorld msg="Phoenix Storage" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
