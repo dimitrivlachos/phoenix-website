9b40aad48c69cd7b6a831c4abdf0f7ec3c44933c
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-10-04 00:14:50 +0100

    Added download button

--- phoenix/src/components/HomeContent.vue
+++ phoenix/src/components/HomeContent.vue
@@ -14,14 +14,42 @@
         >Instagram</a
       >!
     </p>
+    <h3>Downloadable Documents</h3>
+    <button
+      @click="
+        downloadItem({
+          url: 'https://github.com/dimitrivlachos/phoenix-website/blob/main/phoenix/src/assets/Phoenix6018%20Rental%20Agreement.pdf',
+          label: 'Phoenix Rental Agreement',
+        })
+      "
+    >
+      Phoenix Rental Agreement
+    </button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "HomeContent",
   props: {
     msg: String,
+  },
+  methods: {
+    async downloadItem({ url, label }) {
+      axios
+        .get(url, { responseType: "blob" })
+        .then((response) => {
+          const blob = new Blob([response.data], { type: "application/pdf" });
+          const link = document.createElement("a");
+          link.href = URL.createObjectURL(blob);
+          link.download = label;
+          link.click();
+          URL.revokeObjectURL(link.href);
+        })
+        .catch(console.error);
+    },
   },
 };
 </script>
