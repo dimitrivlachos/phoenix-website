7d647e66b0b08b1b158e9cf865e53734fc13c40a
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-10-03 20:50:26 +0100

    Renamed helloworld component

--- phoenix/src/views/HomeView.vue
+++ phoenix/src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/Phoenix_logo.jpg" />
-    <HelloWorld msg="Phoenix Storage" />
+    <HomeContent msg="Phoenix Storage" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HomeContent from "@/components/HomeContent.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    HomeContent,
   },
 };
 </script>
