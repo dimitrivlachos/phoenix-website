a14b1adad6f7b83832782d3b85ab0697371731e1
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-10-03 23:23:49 +0100

    Added contact page

---
+++ phoenix/src/views/ContactView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Contact">
+    <NavBar />
+    <h1>This is a contact page</h1>
+  </div>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  name: "ContactView",
+  components: {
+    NavBar,
+  },
+};
+</script>
+
+<style scoped></style>
