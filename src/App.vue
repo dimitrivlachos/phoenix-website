13f40930c69c440491ccd343362fd07255e2400b
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-10-05 00:26:46 +0100

    Basic layout using tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Phoenix Logo" src="./assets/Phoenix_logo.png" />
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link :to="{ name: 'about' }">About</router-link>
+  <div id="app" class="flex flex-col h-screen">
+    <!-- Navigation -->
+    <nav class="bg-gray-800 text-white p-4 flex items-center justify-between">
+      <!-- Logo on the left -->
+      <div class="flex items-left">
+        <img src="./assets/Phoenix_logo.png" alt="Logo" class="h-8 w-8 mr-2" />
+      </div>
+      <div class="flex items-center text-2xl">
+        <router-link to="/" class="mx-2">Home</router-link> |
+        <router-link :to="{ name: 'about' }" class="mx-2">About</router-link>
+      </div>
+
+      <!-- Title on the right -->
+      <div class="text-xl font-bold">
+        <h1>Phoenix Storage</h1>
+      </div>
     </nav>
-    <router-view/>
+
+    <!-- Page contents -->
+    <div class="flex-1">
+      <router-view />
+    </div>
   </div>
 </template>
 
