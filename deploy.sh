cc644e4c940ac8975e905db767dac5438105d1da
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-10-04 17:38:09 +0100

    Fixed deployment script

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:dimitrivlachos/phoenix-website.git main:gh-pages
+git push -f git@github.com:dimitrivlachos/phoenix-website.git master:gh-pages
 
 cd -
