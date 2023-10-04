959262c1c8b6f504a8becaaf3d4938700c1b884b
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-10-04 23:52:15 +0100

    Added tailwind

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-//import "./assets/css/tailwind.css";
-import "./assets/css/main.css";
+import "./assets/tailwind.css";
 
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import './assets/tailwind.css'
 
 createApp(App).use(router).mount("#app");
