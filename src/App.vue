2d15a6b2c76c05f6e247ce1446ed5d11bab7ea5d
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-10-05 20:59:39 +0100

    Moved navbar to component

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,7 @@
 <template>
   <div id="app" class="flex flex-col h-screen">
     <!-- Navigation -->
-    <nav class="bg-gray-800 text-white p-4 flex items-center justify-between">
-      <!-- Logo on the left -->
-      <div class="flex items-left">
-        <img src="./assets/Phoenix_logo.png" alt="Logo" class="h-8 w-8 mr-2" />
-      </div>
-
-      <!-- Links in the middle -->
-      <div class="flex items-center text-2xl">
-        <router-link
-          to="/"
-          class="mx-2 px-4 py-2 border rounded-md text-white border-white hover:bg-white hover:text-gray-800"
-          active-class="bg-white text-gray-800"
-        >
-          Home
-        </router-link>
-        <router-link
-          :to="{ name: 'contact' }"
-          class="mx-2 px-4 py-2 border rounded-md text-white border-white hover:bg-white hover:text-gray-800"
-          active-class="bg-white text-gray-800"
-        >
-          Contact
-        </router-link>
-      </div>
-
-      <!-- Title on the right -->
-      <div class="text-4xl font-bold font-serif">
-        <h1>Phoenix Storage</h1>
-      </div>
-    </nav>
+    <phoenix-header />
 
     <!-- Page contents -->
     <div class="flex-1">
@@ -42,10 +14,7 @@
 
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import PhoenixHeader from '@/components/PhoenixHeader.vue'
 import PhoenixFooter from '@/components/PhoenixFooter.vue'
-
 </script>
 
-<style scoped>
-  
-</style>
---
+++ src/components/PhoenixHeader.vue
@@ -0,0 +1,37 @@
+<template>
+    <nav class="bg-gray-800 text-white p-4 flex items-center justify-between">
+        <!-- Logo on the left -->
+        <div class="flex items-left">
+            <img src="@/assets/Phoenix_logo.png" alt="Logo" class="h-8 w-8 mr-2" />
+        </div>
+
+        <!-- Links in the middle -->
+        <div class="flex items-center text-2xl">
+            <router-link to="/"
+                class="mx-2 px-4 py-2 border rounded-md text-white border-white hover:bg-white hover:text-gray-800"
+                active-class="bg-white text-gray-800">
+                Home
+            </router-link>
+            <router-link :to="{ name: 'contact' }"
+                class="mx-2 px-4 py-2 border rounded-md text-white border-white hover:bg-white hover:text-gray-800"
+                active-class="bg-white text-gray-800">
+                Contact
+            </router-link>
+        </div>
+
+        <!-- Title on the right -->
+        <div class="text-4xl font-bold font-serif">
+            <h1>Phoenix Storage</h1>
+        </div>
+    </nav>
+</template>
+  
+<script>
+import RouterLink from 'vue-router'
+
+export default {
+    components: {
+        RouterLink,
+    },
+}
+</script>
