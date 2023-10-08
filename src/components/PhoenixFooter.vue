69b461ac3971a2970bab41aed78162a7195b59a0
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-10-08 21:15:07 +0100

    Regularised margin

--- src/components/PhoenixFooter.vue
+++ src/components/PhoenixFooter.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="pb-1">
+    <div>
         <footer class="bg-white rounded-lg shadow dark:bg-gray-800 m-4">
             <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div class="sm:flex sm:items-center sm:justify-between">
