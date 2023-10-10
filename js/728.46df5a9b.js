d6a762fec12ed9462ced634414d9eb4529156bfa
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-10-10 23:50:32 +0100

    deploy

---
+++ js/728.46df5a9b.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkphoenix_website"]=self["webpackChunkphoenix_website"]||[]).push([[728],{2728:function(e,t,n){n.r(t),n.d(t,{default:function(){return f}});var s=n(6252);const o={class:"text-center mt-12"},r=(0,s._)("h1",{class:"text-5xl mb-5"},"404 - Page Not Found",-1),a=(0,s._)("p",{class:"text-2xl"},"The page you are looking for does not exist 🥺",-1),c=[r,a];function i(e,t){return(0,s.wg)(),(0,s.iD)("div",o,c)}var u=n(3744);const l={},x=(0,u.Z)(l,[["render",i]]);var f=x}}]);
+//# sourceMappingURL=728.46df5a9b.js.map
